840e622478df6edbf2a57421d5fecd960cc4be05
M'timet Mohamed Ali <mohamedali.mtimet@gmail.com>
2016-05-31 11:15:11 +0100

    Conclusion + minor changes for prepare module

--- demo/prepare_data_for_sklearn.py
+++ demo/prepare_data_for_sklearn.py
@@ -1,13 +1,13 @@
 #Adapt Data for Scikit Learn model fitting and predict
 def prepare_data_for_sklearn(df, is_test=False):
-    x_columns = ['Hour','Month','Year','X','Y','DayOfWeek']
+    x_columns = ['Hour','Month','Year','X','Y']
     y_column = 'Category'   
-    #x_categorical_columns = ['']
-       #x_columns.extend(x_categorical_columns)
+    x_categorical_columns = ['DayOfWeek']
+    x_columns.extend(x_categorical_columns)
     
     X = df[x_columns]
-    #if len(x_categorical_columns)>0:
-       # X = pd.get_dummies(X, columns=x_categorical_columns)
+    if len(x_categorical_columns)>0:
+        X = pd.get_dummies(X, columns=x_categorical_columns)
     
     y = None if is_test else df[y_column].values
     
